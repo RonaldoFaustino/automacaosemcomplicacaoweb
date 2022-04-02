f1517c36aa582a453ccbe8c402f01ced29a808f3
ohnir <ohnirucse@msn.com>
2022-04-01 23:37:30 -0300

    loginPage

--- src/test/java/br/com/chronosacademy/pages/LoginPage.java
+++ src/test/java/br/com/chronosacademy/pages/LoginPage.java
@@ -1,6 +1,8 @@
 package br.com.chronosacademy.pages;
 
+import br.com.chronosacademy.core.Driver;
 import br.com.chronosacademy.maps.LoginMap;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
@@ -8,5 +10,42 @@ public class LoginPage {
 
     public LoginPage() {
         this.loginMap = loginMap;
+        PageFactory.initElements(Driver.getDriver(), loginMap);
+    }
+
+    public void clickBtnLogin() {
+        loginMap.btnlogin.click();
+    }
+
+    public void clickBtnFechar(){
+        loginMap.btnFechar.click();
+    }
+
+    public void clickDivFdecharModal(){
+        loginMap.divFecharModal.click();
+    }
+
+    public void setInpUserName(String username){
+        loginMap.inpUserName.sendKeys(username);
+    }
+
+    public void setInpPassWord(String password){
+        loginMap.inpPassword.sendKeys(password);
+    }
+
+    public void clickInpRemember(){
+        loginMap.inpRemember.click();
+    }
+
+    public void clickBtnSingIn(){
+        loginMap.btnSingIn.click();
+    }
+
+    public void clickCreateAccount(){
+        loginMap.LinkCreatAccount.click();
+    }
+
+    public boolean isBtnSingIN(){
+        return loginMap.btnSingIn.isEnabled();
     }
 }
